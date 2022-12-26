4405e9153a0c575c3deeaeac6c13ec7e5cf9c7d6
Youngone Lee <youngone.lee@accenture.com>
2022-12-25 20:15:34 -0600

    fixed the arrow movements so they are somewhat relative to the blcoks and not hardcoded

--- Assets/Scripts/ArrowScript.cs
+++ Assets/Scripts/ArrowScript.cs
@@ -6,23 +6,28 @@ public class ArrowScript : MonoBehaviour
 {
     public GameObject arrowObject;
 
+    public Transform block2l, block4l;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        //block2l = GameObject.Find("Block2").transform.position.y;
+        //block4l = GameObject.Find("Block4");
         arrowObject = GameObject.Find("Arrow");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("block2l y value " + block2l.position.y);
     }
 
     public void clickedButtonDown()
     {
         if (!Input.GetKeyDown("space"))
         {
-            arrowObject.transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
+            arrowObject.transform.position = new Vector3(block4l.position.x + 2.5f, block4l.position.y, block4l.position.z);
         }
     }
 
@@ -30,7 +35,7 @@ public class ArrowScript : MonoBehaviour
     {
         if (!Input.GetKeyDown("space"))
         {
-            arrowObject.transform.position = new Vector3(transform.position.x, 2.5f, transform.position.z);
+            arrowObject.transform.position = new Vector3(block2l.position.x + 2.5f, block2l.position.y, block2l.position.z);
         }
     }
 }
