7add9bbbc25d61d21752ec0a6cb8ed3ea684d6c3
Youngone Lee <youngone.lee@accenture.com>
2022-12-25 21:03:20 -0600

    bool logic needs polishing + not use the update function for lag sake but the transform swapping positions work

--- Assets/Scripts/IceRelated.cs
+++ Assets/Scripts/IceRelated.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class IceRelated : MonoBehaviour
 {
     private GameObject cube1, cube2, cube3, cube4;
+    //public Transform block1, block2, block3, block4;
     [SerializeField] SpriteRenderer spriteRenderer;
+
+    public bool flipA = false;
+    public bool flipB = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +25,52 @@ public class IceRelated : MonoBehaviour
     {
         if (!Input.GetKeyDown("space"))
         {
+            //visuals flipping sprites (FOR NOW)
             if (spriteRenderer.flipX) spriteRenderer.flipX = false;
             else spriteRenderer.flipX = true;
+
+            //swapping position boolean logic
+            if (!(flipA && flipB))
+            {
+                flipA = true;
+                //swappingIcePosition(flipA, flipB);
+            }
+            if(flipA)
+            {
+                flipA = false;
+                flipB = true;
+                //swappingIcePosition(flipA, flipB);
+            }
+            if (flipB)
+            {
+                flipA = true;
+                flipB = false;
+                //swappingIcePosition(flipA, flipB);
+            } else
+            {
+                Debug.Log("missing case yikes");
+            }
         }
     }
+
+    //public void swappingIcePosition(bool flagA, bool flagB)
+    //{
+    //    if (flagA)
+    //    {
+    //        //swap 1 and 2
+    //        Vector3 temporaryPos1 = cube1.transform.position;
+    //        cube1.transform.position = cube2.transform.position;
+    //        cube2.transform.position = temporaryPos1;
+
+    //        //swap 3 and 4
+    //        Vector3 temporaryPos3 = cube3.transform.position;
+    //        cube3.transform.position = cube4.transform.position;
+    //        cube4.transform.position = temporaryPos3;
+    //    }
+    //    if (flagB)
+    //    {
+    //        //swap 2 and 1
+    //        //swap 4 and 3
+    //    }
+    //}
 }
--- Assets/Scripts/PenguinScript.cs
+++ Assets/Scripts/PenguinScript.cs
@@ -5,21 +5,79 @@ using UnityEngine;
 public class PenguinScript : MonoBehaviour
 {
     public GameObject happyCat;
+
+    //accessing iceRelated script
+    IceRelated iceRelatedS;
+    [SerializeField] GameObject iceRel;
+
+    //positions of the blocks
+    public Transform block1, block2, block3, block4;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        iceRelatedS = iceRel.GetComponent<IceRelated>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!(iceRelatedS.flipA && iceRelatedS.flipB))
+        {
+            swappingIcePosition(iceRelatedS.flipA, iceRelatedS.flipB);
+        }
+        if (iceRelatedS.flipA)
+        {
+            iceRelatedS.flipA = false;
+            iceRelatedS.flipB = true;
+            swappingIcePosition(iceRelatedS.flipA, iceRelatedS.flipB);
+        }
+        if (iceRelatedS.flipB)
+        {
+            iceRelatedS.flipA = true;
+            iceRelatedS.flipB = false;
+            swappingIcePosition(iceRelatedS.flipA, iceRelatedS.flipB);
+        }
+        else
+        {
+            Debug.Log("missed case");
+        }
     }
+
 
     public void GoodAnimation()
     {
         Debug.Log("proper hit");
         Instantiate(happyCat, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+    }
+
+    public void swappingIcePosition(bool flagA, bool flagB)
+    {
+        if (flagA)
+        {
+            //swap 1 and 2
+            Vector3 temporaryPos1 = block1.transform.position;
+            block1.transform.position = block2.transform.position;
+            block2.transform.position = temporaryPos1;
+
+            //swap 3 and 4
+            Vector3 temporaryPos3 = block3.transform.position;
+            block3.transform.position = block4.transform.position;
+            block4.transform.position = temporaryPos3;
+        }
+        if (flagB)
+        {
+            //swap 2 and 1
+            Vector3 temporaryPos2 = block2.transform.position;
+            block2.transform.position = block1.transform.position;
+            block1.transform.position = temporaryPos2;
+
+            //swap 4 and 3
+            Vector3 temporaryPos4 = block4.transform.position;
+            block4.transform.position = block3.transform.position;
+            block3.transform.position = temporaryPos4;
+        }
     }
 }
